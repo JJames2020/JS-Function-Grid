7eb3326f8953b19efd016837d7e178ffb73b9ff2
Jason James <jjames2020@bklawtech.com>
2017-02-07 21:06:21 +0000

    done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //Write your function calls below this line
+setColor(0, 0, "red");
+setColor(9, 0, "red");
+setColor(0, 9, "red");
+setColor(9, 9, "red");
+setColor(2,2, "black");
+setColor(3, 2, "black");
+setColor(2, 7, "black");
+setColor(3,7, "black");
+setColor(5, 2, "black");
+setColor(6, 3, "black");
+setColor(7, 4, "black");
+setColor(7, 5, "black");
+setColor(6, 6, "black");
+setColor(5, 7, "black");
+
